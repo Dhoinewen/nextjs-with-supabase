0d3cb2db0a6a7898a1de7dd2cf7d9804627909af
maxims <pogchamp1401@gmail.com>
2025-05-17 19:04:11 +0300

    updated header

--- components/header-auth-client.tsx
+++ components/header-auth-client.tsx
@@ -1,72 +1,45 @@
-'use client';
+"use client";
 
 import { signOutAction } from "@/app/actions";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import Link from "next/link";
-import { Badge } from "./ui/badge";
 import { Button } from "./ui/button";
 import { useUser } from "@/context/UserContext";
+import { LogOut, User } from "lucide-react";
+import { paths } from "@/utils/paths";
 
 export default function AuthButtonClient() {
   const { user, isLoading } = useUser();
 
-  if (!hasEnvVars) {
-    return (
-      <>
-        <div className="flex gap-4 items-center">
-          <div className="flex gap-2">
-            <Button
-              asChild
-              size="sm"
-              variant={"outline"}
-              disabled
-              className="opacity-75 cursor-none pointer-events-none"
-            >
-              <Link href="/sign-in">Sign in</Link>
-            </Button>
-            <Button
-              asChild
-              size="sm"
-              variant={"default"}
-              disabled
-              className="opacity-75 cursor-none pointer-events-none"
-            >
-              <Link href="/sign-up">Sign up</Link>
-            </Button>
-          </div>
-        </div>
-      </>
-    );
-  }
-
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <div className="flex items-center justify-center">
+      <div className="text-center">
+        <div className="animate-spin rounded-full h-4 w-4 border-t-2 border-b-2 border-primary mx-auto"></div>
+      </div>
+    </div>;
   }
 
   return user ? (
     <div className="flex items-center gap-4">
-      Hey, {user.email}!
       <div className="flex gap-2">
         <Button asChild size="sm" variant={"secondary"}>
-          <Link href="/cats">Cat Images</Link>
+          <Link href={paths.profile}>
+            <User />
+          </Link>
         </Button>
         <form action={signOutAction}>
-          <Button type="submit" variant={"outline"}>
-            Sign out
+          <Button type="submit" size={"sm"} variant={"outline"}>
+            <LogOut />
           </Button>
         </form>
       </div>
     </div>
   ) : (
     <div className="flex gap-2">
-      <Button asChild size="sm" variant={"secondary"}>
-        <Link href="/cats">Cats List</Link>
-      </Button>
       <Button asChild size="sm" variant={"outline"}>
-        <Link href="/sign-in">Sign in</Link>
+        <Link href={paths.signIn}>Sign in</Link>
       </Button>
       <Button asChild size="sm" variant={"default"}>
-        <Link href="/sign-up">Sign up</Link>
+        <Link href={paths.signUp}>Sign up</Link>
       </Button>
     </div>
   );
