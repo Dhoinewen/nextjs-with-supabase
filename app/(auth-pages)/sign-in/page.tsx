d49daa298b911e43a8f75f9bb64eb1101ce26c12
maxims <pogchamp1401@gmail.com>
2025-05-18 04:26:08 +0300

    delete unused

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { signInAction } from "@/app/actions";
-import { FormMessage, Message } from "@/components/form-message";
+import { FormMessage } from "@/components/form-message";
 import { SubmitButton } from "@/components/submit-button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
